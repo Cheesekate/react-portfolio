3dbcd9079d43f146b271556c3b4a79ffbc4d567d
Kathryn wade <katewade0@gmail.com>
2020-07-13 19:42:43 -0700

    routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Nav from 'react-bootstrap/Nav'
 import './App.css';
 
 import Footer from './components/Footer';
+import HomePage from './components/HomePage';
 
 
 class App extends React.Component {
@@ -48,9 +49,15 @@ class App extends React.Component {
                 <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/about">About</Link>
                 <Link className="nav-link" to="/contact">Contact</Link>
+
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+
 
           <Footer />
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
     return (
         <footer className="mt-5">
             <Container fluid={true}>
-                <Row className="border-top justify-content-between p-3">
+                <Row className="border-top center-content-between p-3">
                     <Col className="p-0" md={3} sm={12}>
                         Kate Wade
                 </Col>
