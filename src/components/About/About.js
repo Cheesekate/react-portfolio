cfc0532a779bca3ae63971779582b293ba6b3ef6
Kathryn wade <katewade0@gmail.com>
2020-07-16 20:21:14 -0700

    about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 
 export default function About(props) {
     return (
         <div>
-            <C
-            
+            <Container fluid={true}>
+                <Row className="justify-content-center">
+                    <Col md={8}>
+                        {props.children}
+                    </Col>
+                </Row>
+            </Container>
+
         </div>
     )
 }
