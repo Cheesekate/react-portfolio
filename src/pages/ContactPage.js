da58a0fe3fab2e2c95dd71e6af770980f35943c9
Kathryn wade <katewade0@gmail.com>
2020-07-16 20:28:48 -0700

    Contact Page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,85 @@
-import React from 'react';
+import React from "react";
 
-export default function ContactPage() {
-    return (
-        <div>
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-        </div>
-    )
+import Hero from '../components/Hero';
+import Content from '../components/About/About';
+
+
+import axios from "axios";
+
+
+class ContactUs extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            name: '',
+            message: '',
+            disabled: false,
+            emailSent: null,
+        }
+
+    }
+
+    handleChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        this.setState({
+            disabled: true,
+        });
+
+
+    }
+
+    render() {
+        return (
+            <>
+                <h1> Contact Us</h1>
+
+                <Content>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} placeholder="Enter email" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicName">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control id="name" name="name" type="text" value={this.state.name} onChange={this.handleChange} placeholder="Name" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicMessage">
+                            <Form.Label>Message</Form.Label>
+                            <Form.Control id="message" name="message" as="textarea" rows="3" value={this.state.message} onChange={this.handleChange} placeholder="Message" />
+                        </Form.Group>
+
+                        {/* <Form.Group controlId="formBasicCheckbox"></Form.Group> */}
+                        <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>
+                            Send
+						</Button>
+
+                        {this.state.emailSent === true && <p className="d-inline success-msg"> Email Sent</p>}
+                        {this.state.emailSent === false && <p className="d-inline err-msg"> Email Sent</p>}
+
+                    </Form>{" "}
+
+                </Content>
+            </>
+
+
+        );
+    }
 }
+
+export default ContactUs;
